07a5002a8625abe4c04f39a896122d609a848f1f
Shivam sood <155950691+Shivam-sood63@users.noreply.github.com>
2025-06-14 10:53:43 +0530

    React

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Portfolio
-React.js
+React and Tailwind
---
+++ src/Context/AppContext.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import Navbar from '../Components/Navbar'
+
+
+// const AppContext = (props) => {
+ 
+  
+//        console.log(props);
+//        return props.children;
+       
+   
+// }
+
+// export default AppContext
